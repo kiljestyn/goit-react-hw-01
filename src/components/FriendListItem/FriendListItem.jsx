65f8106c7fe455353c162db2c23489a8d5b2ed2e
Marta Kiljestyn <kiljestyn@gmail.com>
2024-03-26 15:00:55 +0200

    task2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,25 @@
 import css from "./friendListItem.module.css";
 
-const FriendListItem = ({ avatar, name, isOnline, id }) => {
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  //   return (
+  //     <li className={css.item}>
+  //       <span
+  //         className={isOnline ? css.status_online : css.status_offline}
+  //       ></span>
+  //       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+  //       <p className={css.name}>{name}</p>
+  //     </li>
+  //   );
+  // };
   return (
-    <li key={id} className={css.item}>
-      <span
-        className={isOnline ? css.status_online : css.status_offline}
-      ></span>
+    <li className={css.item}>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
+      {isOnline ? (
+        <p className={css.online}>Online</p>
+      ) : (
+        <p className={css.offline}>Offline</p>
+      )}
     </li>
   );
 };
